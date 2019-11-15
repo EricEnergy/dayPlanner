0ed343d22833215341e117879932bf80081bf044
Eric Energy <ericnrgnash@gmail.com>
2019-11-14 22:12:11 -0600

    Layout set.

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-var date = '11-14-2019';
-var format = 'LLLL';
-var result = moment(date).format(format);
-console.log(result);
-$('#currentDay').text(date);
 
 var workHours = ['9AM' ,'10AM' ,'11AM' ,'12PM' ,'1PM' ,'2PM' ,'3PM' ,'4PM' ,'5PM' ];
+workHours.forEach(function(item){
 
-workHours.forEach(function(item, index, array){
-
-    var HR = $(".container").append("<hr>");
-    var rowMaker = $(".container").append("<div class=row>");
-    var i = 0;
-
-    i++;
-    $(".row").append(item);
-
-
-    console.log(item);
-    console.log(index);
-    console.log(array);
+    $('.container').append('<hr>');
+    $('.container').append('<div class="row">' + `<div class="col-md-2">${item}</div>` + '<input class="col-md-9 info">' + '<div class="col-md-1">0</div>' +'</div>');
 });
+
 
 // 2 6 2
 
 //col-md-6
+
+
