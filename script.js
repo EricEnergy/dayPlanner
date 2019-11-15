047f9c8511beb61b0b666c46c62e0178b22773ae
Eric Energy <ericnrgnash@gmail.com>
2019-11-14 22:52:41 -0600

    timer and layout added.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+
 var workHours = ['9AM' ,'10AM' ,'11AM' ,'12PM' ,'1PM' ,'2PM' ,'3PM' ,'4PM' ,'5PM' ];
 workHours.forEach(function(item){
 
@@ -6,9 +8,9 @@ workHours.forEach(function(item){
     $('.container').append('<div class="row">' + `<div class="col-md-2">${item}</div>` + '<input class="col-md-9 info">' + '<div class="col-md-1">0</div>' +'</div>');
 });
 
+setInterval(() => {
 
-// 2 6 2
-
-//col-md-6
-
+    var now = moment().format("MMM, Do, h:mm:ss a");
+    var nowPlacement = $('#currentDay').text(now);
+},1000);
 
